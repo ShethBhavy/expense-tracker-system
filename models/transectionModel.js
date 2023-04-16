96be28591c8a14d2eb4049378cb0e03051b36571
ShethBhavy <bhavy5282@gmail.com>
2023-04-16 15:06:12 +0530

    Other issue

--- models/transectionModel.js
+++ models/transectionModel.js
@@ -18,17 +18,18 @@ const transectionSchema = new mongoose.Schema(
             type: String,
             requires: [true, "cat is required"],
         },
+        date: {
+            type: Date,
+            required: [true, "data is required"],
+        },
         refrence: {
             type: String,
         },
         description: {
             type: String,
-            required: [true, "desc is required"],
+            
         },
-        date: {
-            type: Date,
-            required: [true, "data is required"],
-        },
+        
     },
     { timestamps: true }
 );
